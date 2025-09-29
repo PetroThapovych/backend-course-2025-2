c1c350ef70f0fbc2c327d1276f9414c8becb7d7c
Petro_Thapovych <capovicp@gmail.com>
2025-09-29 18:04:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const currency = "JPY";
-const exact_date = "20240127"; // формат YYYYMMDD
+const exact_date = "20240127"; 
 
 let url = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
 
 const params = new URLSearchParams();
 params.append("valcode", currency);
 params.append("date", exact_date);
-params.append("json", ""); // параметр без значення
+params.append("json", ""); 
 
 url.search = params.toString();
 
 console.log(url.toString());
+
